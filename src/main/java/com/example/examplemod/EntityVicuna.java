a68dcf51bd0a748efcf25cbef9a7946cba98b2cc
IGoonyI <31140844+IGoonyI@users.noreply.github.com>
2017-10-16 19:35:13 -0400

    Fix4

--- src/main/java/com/example/examplemod/EntityVicuna.java
+++ src/main/java/com/example/examplemod/EntityVicuna.java
@@ -5,9 +5,11 @@ import javax.annotation.Nullable;
 import com.google.common.base.Predicate;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockTallGrass;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.state.pattern.BlockStateMatcher;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.EntityLivingBase;
@@ -15,6 +17,8 @@ import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.IRangedAttackMob;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackRanged;
+import net.minecraft.entity.ai.EntityAIAvoidEntity;
+import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAIFollowParent;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAILlamaFollowCaravan;
@@ -26,6 +30,7 @@ import net.minecraft.entity.ai.EntityAIRunAroundLikeCrazy;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWanderAvoidWater;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.ai.RandomPositionGenerator;
 import net.minecraft.entity.passive.AbstractChestHorse;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.passive.EntityWolf;
@@ -59,10 +64,20 @@ import java.util.List;
 import javax.annotation.Nullable;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.pathfinding.Path;
 import net.minecraft.pathfinding.PathNavigate;
 import net.minecraft.util.EntitySelectors;
 import net.minecraft.util.math.Vec3d;
+import javax.annotation.Nullable;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
 
 public class EntityVicuna extends AbstractChestHorse implements IRangedAttackMob
 {
@@ -378,5 +393,119 @@ public class EntityVicuna extends AbstractChestHorse implements IRangedAttackMob
 		// TODO Auto-generated method stub
 		
 	}
+	public class EntityAIAvoidEntity<T extends Entity> extends EntityAIBase
+	{
+	    private final Predicate<Entity> canBeSeenSelector;
+	    /** The entity we are attached to */
+	    protected EntityCreature entity;
+	    private final double farSpeed;
+	    private final double nearSpeed;
+	    protected T closestLivingEntity;
+	    private final float avoidDistance;
+	    /** The PathEntity of our entity */
+	    private Path entityPathEntity;
+	    /** The PathNavigate of our entity */
+	    private final PathNavigate entityPathNavigate;
+	    /** Class of entity this behavior seeks to avoid */
+	    private final Class<T> classToAvoid;
+	    private final Predicate <? super T > avoidTargetSelector;
+
+	    public EntityAIAvoidEntity(EntityCreature entityIn, Class<T> classToAvoidIn, float avoidDistanceIn, double farSpeedIn, double nearSpeedIn)
+	    {
+	        this(entityIn, classToAvoidIn, Predicates.alwaysTrue(), avoidDistanceIn, farSpeedIn, nearSpeedIn);
+	    }
+
+	    public EntityAIAvoidEntity(EntityCreature entityIn, Class<T> classToAvoidIn, Predicate <? super T > avoidTargetSelectorIn, float avoidDistanceIn, double farSpeedIn, double nearSpeedIn)
+	    {
+	        this.canBeSeenSelector = new Predicate<Entity>()
+	        {
+	            public boolean apply(@Nullable Entity p_apply_1_)
+	            {
+	                return p_apply_1_.isEntityAlive() && EntityAIAvoidEntity.this.entity.getEntitySenses().canSee(p_apply_1_) && !EntityAIAvoidEntity.this.entity.isOnSameTeam(p_apply_1_);
+	            }
+	        };
+	        this.entity = entityIn;
+	        this.classToAvoid = classToAvoidIn;
+	        this.avoidTargetSelector = avoidTargetSelectorIn;
+	        this.avoidDistance = avoidDistanceIn;
+	        this.farSpeed = farSpeedIn;
+	        this.nearSpeed = nearSpeedIn;
+	        this.entityPathNavigate = entityIn.getNavigator();
+	        this.setMutexBits(1);
+	    }
+
+	    /**
+	     * Returns whether the EntityAIBase should begin execution.
+	     */
+	    public boolean shouldExecute()
+	    {
+	        List<T> list = this.entity.world.<T>getEntitiesWithinAABB(this.classToAvoid, this.entity.getEntityBoundingBox().grow((double)this.avoidDistance, 3.0D, (double)this.avoidDistance), Predicates.and(EntitySelectors.CAN_AI_TARGET, this.canBeSeenSelector, this.avoidTargetSelector));
+
+	        if (list.isEmpty())
+	        {
+	            return false;
+	        }
+	        else
+	        {
+	            this.closestLivingEntity = list.get(0);
+	            Vec3d vec3d = RandomPositionGenerator.findRandomTargetBlockAwayFrom(this.entity, 16, 7, new Vec3d(this.closestLivingEntity.posX, this.closestLivingEntity.posY, this.closestLivingEntity.posZ));
+
+	            if (vec3d == null)
+	            {
+	                return false;
+	            }
+	            else if (this.closestLivingEntity.getDistanceSq(vec3d.x, vec3d.y, vec3d.z) < this.closestLivingEntity.getDistanceSqToEntity(this.entity))
+	            {
+	                return false;
+	            }
+	            else
+	            {
+	                this.entityPathEntity = this.entityPathNavigate.getPathToXYZ(vec3d.x, vec3d.y, vec3d.z);
+	                return this.entityPathEntity != null;
+	            }
+	        }
+	    }
+
+	    /**
+	     * Returns whether an in-progress EntityAIBase should continue executing
+	     */
+	    public boolean shouldContinueExecuting()
+	    {
+	        return !this.entityPathNavigate.noPath();
+	    }
+
+	    /**
+	     * Execute a one shot task or start executing a continuous task
+	     */
+	    public void startExecuting()
+	    {
+	        this.entityPathNavigate.setPath(this.entityPathEntity, this.farSpeed);
+	    }
+
+	    /**
+	     * Reset the task's internal state. Called when this task is interrupted by another one
+	     */
+	    public void resetTask()
+	    {
+	        this.closestLivingEntity = null;
+	    }
+
+	    /**
+	     * Keep ticking a continuous task that has already been started
+	     */
+	    public void updateTask()
+	    {
+	        if (this.entity.getDistanceSqToEntity(this.closestLivingEntity) < 49.0D)
+	        {
+	            this.entity.getNavigator().setSpeed(this.nearSpeed);
+	        }
+	        else
+	        {
+	            this.entity.getNavigator().setSpeed(this.farSpeed);
+	        }
+	    }
+	}
 	
 }
+
+
