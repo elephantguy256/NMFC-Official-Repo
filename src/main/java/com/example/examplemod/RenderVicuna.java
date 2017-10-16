a3b06b7a68acb2df97341ae1e48a8f232fd12ef5
IGoonyI <31140844+IGoonyI@users.noreply.github.com>
2017-10-15 22:48:28 -0400

    Animations
    Anim

--- src/main/java/com/example/examplemod/RenderVicuna.java
+++ src/main/java/com/example/examplemod/RenderVicuna.java
@@ -1,6 +1,7 @@
 package com.example.examplemod;
 
 import net.minecraft.client.model.ModelLlama;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.layers.LayerLlamaDecor;
@@ -13,12 +14,15 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class RenderVicuna extends RenderLiving<EntityVicuna>
 {
     private static final ResourceLocation[] LLAMA_TEXTURES = new ResourceLocation[] {new ResourceLocation("de:textures/entity/preyanimal/vicuna.png"), new ResourceLocation("de:textures/entity/preyanimal/vicuna2.png")};
+	
 
     public RenderVicuna(RenderManager p_i47203_1_, ModelVicuna modelVicuna, float f)
     {
         super(p_i47203_1_, new ModelVicuna(), 0.7F);
         
     }
+   
+  
 
     /**
      * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
@@ -27,4 +31,5 @@ public class RenderVicuna extends RenderLiving<EntityVicuna>
     {
         return LLAMA_TEXTURES[entity.getVariant()];
     }
+    
 }
